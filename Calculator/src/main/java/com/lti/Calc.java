1d5afab4d300ab2690e5fbc007d8c8f61885255a
Mohan Naik <banothmohan36@gmail.com>
2020-12-21 15:00:53 +0530

    third commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,8 +5,13 @@ public class Calc {
 		return num1 + num2;
 	}
 
+	public static int sub(int num1, int num2) {
+		return num1 + num2;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(add(10, 50));
+		System.out.println(add(10, 60));
+		System.out.println(sub(10, 60));
 	}
 
 }
